8908912cd94360ea426b3d8d8a4b3591b94762de
sgx <495462724@qq.com>
2014-12-28 23:43:38 +0800

    增加ExcelUtils解析

--- ExcelUtils/src/test/java/pw/agiledev/e2e/ExcelToEntity/AppTest.java
+++ ExcelUtils/src/test/java/pw/agiledev/e2e/ExcelToEntity/AppTest.java
@@ -9,6 +9,13 @@ import pw.agiledev.e2e.exception.ExcelRegexpValidFailedException;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+
+import static cn.jimmyshi.beanquery.BeanQuery.select;
+import static cn.jimmyshi.beanquery.BeanQuery.value;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.startsWith;
+
 /**
  * 功能说明： 单元测试
  * 参数说明：
@@ -48,15 +55,16 @@ public class AppTest extends TestCase {
 	public static void main(String[] args) throws IOException, InvalidFormatException {
 		ExcelHelper eh = ExcelHelper.readExcel("F://model.xlsx");
 
-		String[] headers = eh.getHeaders();
-		String[][] datas = eh.getDatas();
+			String[] headers = eh.getHeaders();
+			String[][] datas = eh.getDatas();
 
-		List<Demo> entitys  = null;
-		try {
-			entitys = eh.toEntitys(Demo.class);
-			for (Demo d : entitys) {
-				System.out.println(d.toString());
-			}
+			List<Demo> entitys  = null;
+			try {
+				entitys = eh.toEntitys(Demo.class);
+				List<Map<String,Object>> demos = select("propName","innerName").from(entitys).where(allOf(value("propName", startsWith("制造商")))).execute();
+				for(Map<String,Object> demo : demos) {
+					System.out.println(demo.get("propName") + " : " + demo.get("innerName"));
+				}
 		} catch (ExcelParseException e) {
 			System.out.println(e.getCause().getLocalizedMessage());
 		} catch (ExcelContentInvalidException e) {
