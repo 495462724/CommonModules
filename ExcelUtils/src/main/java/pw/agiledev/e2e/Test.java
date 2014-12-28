07a4586ba82dd8f97c36d6db11da2b3aa07f4ef3
sgx <495462724@qq.com>
2014-12-28 21:46:37 +0800

    增加ExcelUtils解析

---
+++ ExcelUtils/src/main/java/pw/agiledev/e2e/Test.java
@@ -0,0 +1,10 @@
+package pw.agiledev.e2e;
+
+/**
+ * Created by sunguoxing on 2014/12/28.
+ */
+public class Test {
+
+
+
+}
---
+++ ExcelUtils/src/test/java/pw/agiledev/e2e/ExcelToEntity/AppTest.java
@@ -0,0 +1,69 @@
+package pw.agiledev.e2e.ExcelToEntity;
+
+import junit.framework.TestCase;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import pw.agiledev.e2e.ExcelHelper;
+import pw.agiledev.e2e.exception.ExcelContentInvalidException;
+import pw.agiledev.e2e.exception.ExcelParseException;
+import pw.agiledev.e2e.exception.ExcelRegexpValidFailedException;
+
+import java.io.IOException;
+import java.util.List;
+/**
+ * 功能说明： 单元测试
+ * 参数说明：
+ * @author 管宜尧
+ * 2013-11-28 下午8:56:42
+ */
+public class AppTest extends TestCase {
+	public AppTest() throws ExcelParseException{
+		ExcelHelper.registerNewType(MyDataType.class);
+		ExcelHelper.registerNewType(MyDataType2.class);
+	}
+	
+    public void testApp() throws InvalidFormatException, IOException{
+    	ExcelHelper eh = ExcelHelper.readExcel("F://model.xlsx");
+		String[] headers = eh.getHeaders();
+		String[][] datas = eh.getDatas();
+		
+		List<Demo> entitys  = null;
+		try {
+			entitys = eh.toEntitys(Demo.class);
+			for (Demo d : entitys) {
+				System.out.println(d.toString());
+			}
+		} catch (ExcelParseException e) {
+			System.out.println(e.getMessage());
+		} catch (ExcelContentInvalidException e) {
+			System.out.println(e.getMessage());
+		} catch (ExcelRegexpValidFailedException e) {
+			System.out.println(e.getMessage());
+		}
+		
+//		Assert.assertEquals(5, headers.length);
+//		Assert.assertEquals(3, datas.length);
+		//Assert.assertNotNull(entitys);
+    }
+
+	public static void main(String[] args) throws IOException, InvalidFormatException {
+		ExcelHelper eh = ExcelHelper.readExcel("F://model.xlsx");
+
+		String[] headers = eh.getHeaders();
+		String[][] datas = eh.getDatas();
+
+		List<Demo> entitys  = null;
+		try {
+			entitys = eh.toEntitys(Demo.class);
+			for (Demo d : entitys) {
+				System.out.println(d.toString());
+			}
+		} catch (ExcelParseException e) {
+			System.out.println(e.getCause().getLocalizedMessage());
+		} catch (ExcelContentInvalidException e) {
+			System.out.println(e.getMessage());
+		} catch (ExcelRegexpValidFailedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
